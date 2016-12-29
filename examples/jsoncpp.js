e0e4ad1dbf13f437367e5d3e795c44728ad3ab7b
Atte Kettunen <atte.kettunen@f-secure.com>
2016-12-29 13:07:25 +0200

    Update and refactor examples to match new options

--- examples/jsoncpp.js
+++ examples/jsoncpp.js
@@ -1,14 +1,22 @@
-
+#!/usr/bin/env node
 
 module.exports={
+	commandLine:process.env.HOME+'/jsoncpp/build/debug/src/jsontestrunner/jsontestrunner_exe --json-checker @@',
 	debug:false,
-	fileExtension:'json',
-	killTimeout:3000,
-	maxBlockCount:1,
-	filesPerRound:200,
-	inputDirectory:process.env.HOME+'/samples/samples-media-cut/',
-	resultDirectory:process.env.HOME+'/results/',
-	target:'jsoncpp',
-	commandLine:process.env.HOME+'/projects/jsoncpp/build/debug/src/jsontestrunner/jsontestrunner_exe --json-checker @@',
-	tempDirectory:'/run/shm/tmp/'
-}
+    env:{
+        ASAN_OPTIONS:'coverage=1:coverage_dir=\'__workDir__\''
+    },
+    fileExtension:'json',
+    filesPerRound:200,
+    inputDirectory:process.env.HOME+'/samples/samples-json/',
+    killTimeout:3000,
+    maxBlockCount:1,
+    radamsaPath:"radamsa",
+    resultDirectory:process.env.HOME+'/results/',
+    target:'jsoncpp',
+    tempDirectory:'/run/shm/tmp/',
+    testCaseGenerators:[
+        __dirname+'/../testcasegenerators/surku.js',
+        __dirname+'/../testcasegenerators/radamsa.js'
+    ]
+};
--- examples/opusdec.js
+++ examples/opusdec.js
@@ -1,12 +1,24 @@
+#!/usr/bin/env node
 
 module.exports={
-	debug:false,
-	fileExtension:'opus',
-	killTimeout:3000,
-	maxBlockCount:1,
-	inputDirectory:process.env.HOME+'/samples/samples-svg/',
-	resultDirectory:process.env.HOME+'/results/',
-	target:'opusdec',
-	commandLine:process.env.HOME+'/point-n-click/packages/opus-tools/opusdec @@ /dev/null',
-	tempDirectory:'/run/shm/tmp/'
-}
+    commandLine:process.env.HOME+'/opus-tools/opusdec @@ /dev/null',
+    debug:false,
+    env:{
+
+        ASAN_OPTIONS:'coverage=1:coverage_dir=\'__workDir__\''
+
+    },
+    fileExtension:'opus',
+    filesPerRound:200,
+    inputDirectory:process.env.HOME+'/samples/samples-opus/',
+    killTimeout:3000,
+    maxBlockCount:1,
+    radamsaPath:"radamsa",
+    resultDirectory:process.env.HOME+'/results/',
+    target:'opusdec',
+    tempDirectory:'/run/shm/tmp/',
+    testCaseGenerators:[
+        __dirname+'/../testcasegenerators/surku.js',
+        __dirname+'/../testcasegenerators/radamsa.js'
+    ]
+};
